2bb7b2b32a9c8d29df406b7a5b32a81313bf7a35
Heisenberg-15 <42386473+Heisenberg-15@users.noreply.github.com>
2020-10-01 10:08:30 +0530

    Update reducer1.py

--- assignment2/reducer1.py
+++ assignment2/reducer1.py
@@ -2,18 +2,14 @@
 import sys
 path=sys.argv[1]
 d={}
-d1={}
 f=open(path,"w")
 for i in sys.stdin:
 	key,value=i.strip("\t\n").split(" ")
-	if value not in d1:
-		f.write(value+","+"1"+"\n")
-		d1[value]=1
 	if key not in d:
 		d[key]=[int(value)]
-		f.write(key+","+"1"+"\n")
 	else:
 		d[key].append(int(value))
-f.close()
 for i in d:
 	print(i,"\t",d[i],sep="")
+	f.write(i+","+"1"+"\n")
+f.close()

2ea1b92077ee1b57a7bfcac3218b4ee9e0ab2ada
Heisenberg-15 <42386473+Heisenberg-15@users.noreply.github.com>
2020-10-01 10:11:37 +0530

    Update mapper2.py

--- assignment2/mapper2.py
+++ assignment2/mapper2.py
@@ -10,9 +10,9 @@ for i in sys.stdin:
 	key,value=i.strip("\n").split("\t")
 	l=value.strip('[').strip(']').split(",")
 	for j in l:
-		if key in d:
+		if key in d and j in d:
 			print(j.strip(" "),"\t",d[key][0]/len(l),sep="")
 			d[j.strip(" ")][1]=1
 for i in d:
 	if d[i][1]==0:
-		print(i,"\t",0.15,sep="")
+		print(i,"\t",0,sep="")

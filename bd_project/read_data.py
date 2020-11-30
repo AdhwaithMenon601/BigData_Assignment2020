1f47e61f55e0ae112c1847a81ef7ffb7d1df06d0
AdhwaithMenon601 <adhwaith2017.menon@gmail.com>
2020-11-30 14:39:32 +0530

    Added regression dataframe

--- bd_project/read_data.py
+++ bd_project/read_data.py
@@ -4,6 +4,7 @@ from pyspark.streaming import StreamingContext
 from pyspark.context import SparkContext
 from pyspark.sql import SparkSession, Row
 from pyspark.sql.types import *
+from pyspark.sql.functions import datediff,col
 
 # Setting the paths of the CSV files
 play_path = "hdfs://localhost:9000/players.csv"
@@ -15,6 +16,20 @@ player_chemistry = {}
 player_ratings = {}
 profile_schema = None
 player_profile = None
+regr_player = None
+
+def init_regr():
+    regr_schema = StructType([
+        StructField('id', IntegerType(), True),
+        StructField('birth', StringType(), True),
+        StructField('cur', StringType(), True),
+        StructField('rating', FloatType(), True),
+        StructField('diff', IntegerType(), True)
+    ])
+
+    new_df = sp_sess.createDataFrame(sp_sess.sparkContext.emptyRDD(), regr_schema)
+
+    return new_df
 
 # Initialises the schema
 def init_profile():
@@ -78,11 +93,51 @@ def get_profile(player_profile, fouls_per_player, own_per_player, goals_per_play
                 matches = player_profile[player_id][6] + 1
 
 
-            new_list = [int(fouls_per_player[player_id]), player_name, int(goals_per_player), int(own_per_player[player_id]), int(pass_ac[player_id]), int(shots_eff[player_id]), matches]
+            new_list = [int(fouls_per_player[player_id]), player_name, int(goals_per_player), int(own_per_player[player_id]), pass_ac[player_id], int(shots_eff[player_id]), matches]
             player_profile.update({player_id : new_list})
 
     return player_profile
 
+# For Linear regression , preparing dataframe
+def linreg_predict(player_profile, player_ratings, cur_date, teams_dict, regr_player):
+    players = sp_sess.read.csv(play_path, header=True, inferSchema=True)
+
+    player_reg_list = []
+    player_l = []
+
+    r_schema = StructType([
+        StructField('id', IntegerType(), True),
+        StructField('birth', StringType(), True),
+        StructField('cur', StringType(), True),
+        StructField('rating', FloatType(), True),
+        StructField('diff', IntegerType(), True)
+    ])
+
+    for j in teams_dict:
+        for i in teams_dict[j]:
+            player_reg = {}
+            player_l.append(i)
+            player_reg["id"] = i
+            name_df = players.filter(players['Id'] == i)
+            player_date = name_df.select("birthDate").collect()[0].birthDate
+            player_reg["birth"] = player_date
+            player_reg["cur"] = cur_date
+            if (i not in player_ratings):
+                player_ratings.update({i:0})
+            player_reg["rating"] = player_ratings[i]
+            player_reg_list.append(player_reg)
+    
+    player_reg = sp_sess.createDataFrame(player_reg_list, r_schema)
+    temp_df = regr_player.filter(~col("id").isin(player_l))
+    
+    new_reg = player_reg.withColumn("diff",datediff(col("cur"),col("birth")))
+
+    if (not(temp_df.head(1))):
+        final_df = new_reg
+    else:
+        final_df = temp_df.union(new_reg)
+
+    return final_df
 
 def process_record(rdd):
 
@@ -97,6 +152,8 @@ def process_record(rdd):
     event_df = event_json.map(lambda x : Row(**x)).toDF()
 
     match_json = eval(rdd.first())
+    cur_date = match_json["dateutc"]
+    print(cur_date)
 
     # match_df = rdd.filter(lambda x : x == match_json).map(lambda x : eval(x))
     # match_df = match_df.map(lambda x : Row(**x)).toDF()
@@ -106,6 +163,7 @@ def process_record(rdd):
     global player_profile 
     global player_ratings
     global player_chemistry
+    global regr_player
 
     # To insert into the player profile dataframe , we need to create a new df
     # And union this with the player profile
@@ -132,8 +190,12 @@ def process_record(rdd):
     player_ratings = player_rating(player_ratings, player_contribution, own_per_player, fouls_per_player)
     player_chemistry = calc_chemistry(player_chemistry, player_ratings, prev_player_rating, teams_dict)
     player_profile = get_profile(player_profile, fouls_per_player, own_per_player, 3, pass_ac, shots_eff, teams_dict)
+    regr_player = linreg_predict(player_profile, player_ratings, cur_date , teams_dict, regr_player)
+    regr_player.show()
+    """
     for i in player_profile:
         print(player_profile[i])
+    """
     """for it in player_chemistry:
         print("{0:.5f}".format(player_chemistry[it]))"""
     # It is getting updated , but few only are due to the massive size of the pairs dataset
@@ -142,7 +204,7 @@ if __name__ == '__main__':
 
     # Creating the streaming spark context and spark session
     sp_context = SparkContext('local[2]', "Read_Stream")
-    ssp_context = StreamingContext(sp_context, 5)
+    ssp_context = StreamingContext(sp_context, 5.000)
     sp_sess = SparkSession.builder.appName('Read_Data').getOrCreate()
 
     # Adding the files to Spark Context
@@ -162,6 +224,7 @@ if __name__ == '__main__':
     # Initializing the player chem and ratings
     player_chemistry = init_chemistry(players)
     player_ratings = init_ratings(players)
+    regr_player = init_regr()
 
     # Adds player profile
     player_profile = init_profile()

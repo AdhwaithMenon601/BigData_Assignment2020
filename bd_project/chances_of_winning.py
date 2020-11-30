c3f2e5eb132d1860917f56001387fe47ef4263f5
K.Nidarshan <42386473+Heisenberg-15@users.noreply.github.com>
2020-11-30 23:27:52 +0530

    Update chances_of_winning.py

--- bd_project/chances_of_winning.py
+++ bd_project/chances_of_winning.py
@@ -6,9 +6,11 @@ from pyspark.context import SparkContext
 from pyspark.sql import SparkSession, Row
 from pyspark.ml.feature import VectorAssembler
 from pyspark.sql.types import *
+from model import *
 
 
 def prepare_dataframe(player_profile):
+    sp_sess = SparkSession.builder.appName('Read_Data').getOrCreate()
     player_new_profile = []
     tempd = {}
     print(player_profile)
@@ -31,9 +33,10 @@ def prepare_dataframe(player_profile):
     print("THE COUNT IS ", player_df.count())
     return player_df
 
+
 def clustering(player_profile):
     vecAssembler = VectorAssembler(
-        inputCols=["fouls", "goals", "owngoals", "pass_acc", "shots","matches"], outputCol="features")
+        inputCols=["fouls", "goals", "owngoals", "pass_acc", "shots", "matches"], outputCol="features")
     print(player_profile.printSchema())
     player_profile = player_profile.drop("name")
     new_df = vecAssembler.transform(player_profile)
@@ -51,83 +54,84 @@ def clustering(player_profile):
     print("Cluster Centers: ")
     for center in centers:
         print(center)
-    return predictions,centers
+    return predictions, centers
 
 
-def predict(player_chem, player_profile, player_rating, team1, team2):
+def predict(player_chem, player_profile, player_ratings, team1, team2):
     player_coeff1 = {}
-    sp_sess = SparkSession.builder.appName('Read_Data').getOrCreate()
-    lessthan1=[]
+    lessthan1 = []
     for i in team1:
         player = i
         team1.remove(i)
         total1 = 0
-        if player_profile["Matches"]>=5:
-        	for j in team1:
-            		if (i, j) in player_chem:
-                	total1 += player_chem[(i, j)]
-            	else:
-                	total1 += player_chem[(j, i)]
-        	total1 = total1/len(team1)-1
-        	player_coeff1[i] = total1
+        if player_profile[i][6] >= 5:
+            for j in team1:
+                if (i, j) in player_chem:
+                    total1 += player_chem[(i, j)]
+                else:
+                    total1 += player_chem[(j, i)]
+            total1 = total1/len(team1)-1
+            player_coeff1[i] = total1
         else:
-        	lessthan1.append(i)
-    tot1=0
+            lessthan1.append(i)
+    tot1 = 0
     for i in player_coeff1:
-    	tot1+=(player_coeff1[i]*predict_regression(i))
-    player_coeff2={}
-    lessthan2=[]
+        tot1 += (player_coeff1[i]*find_rating(i, "2020-08-11"))
+    player_coeff2 = {}
+    lessthan2 = []
     for i in team2:
         player = i
         team2.remove(i)
         total2 = 0
-        if player_profile["Matches"]>=5:
-        	for j in team2:
-            		if (i, j) in player_chem:
-                	total2 += player_chem[(i, j)]
-            	else:
-                	total2 += player_chem[(j, i)]
-        	total2 = total2/len(team2)-1
-        	player_coeff2[i] = total2
+        if player_profile[i][6] >= 5:
+            for j in team2:
+                if (i, j) in player_chem:
+                    total2 += player_chem[(i, j)]
+                else:
+                    total2 += player_chem[(j, i)]
+            total2 = total2/len(team2)-1
+            player_coeff2[i] = total2
         else:
-        	lessthan2.append(i)
-    tot2=0
+            lessthan2.append(i)
+    tot2 = 0
     for i in player_coeff1:
-    	tot2+=(player_coeff1[i]*predict_regression(i))
-    player_df=prepare_dataframe(player_profile)
-    predictions,centers=clustering(player_df)
+        tot2 += (player_coeff1[i]*find_rating(i, "2020-08-11"))
+    player_df = prepare_dataframe(player_profile)
+    predictions, centers = clustering(player_df)
     for i in lessthan1:
-    	tempdf=predictions.filter(predictions["player_id"]==i)
-    	cluster_id=predictions.filter(predictions["prediction"]==tempdf["prediction"])
-    	temp_ids_df=cluster_id.select("player_id").collect()
-    	temp_ids1=[]
-    	for j in temp_ids_df:
-    		j_dict=j.asDict()
-		temp_ids1.append(j_dict["player_id"])
-	tot=0
-	for k in temp_ids1:
-		tot+=(player_rating[k]*player_chem[k])
-	tot=tot/len(temp_ids_df)
-	tot1+=tot
-	
-    for i in lessthan2:
-    	tempdf=predictions.filter(predictions["player_id"]==i)
-    	cluster_id=predictions.filter(predictions["prediction"]==tempdf["prediction"])
-    	temp_ids_df=cluster_id.select("player_id").collect()
-    	temp_ids2=[]
-    	for j in temp_ids_df:
-    		j_dict=j.asDict()
-		temp_ids2.append(j_dict["player_id"])
-	tot=0
-	for k in temp_ids2:
-		tot+=(player_rating[k]*player_chem[k])
-	tot=tot/len(temp_ids_df)
-	tot2+=tot
-    strength_a=tot1/11
-    strength_b=tot2/11
-    chance_a=(0.5+strength_a-((strength_a+strength_b)/2))*100
-    chance_b=100-chance_a
-    return chance_a,chance_b
-	
-		
+        tempdf = predictions.filter(predictions["player_id"] == i)
+        cluster_id = predictions.filter(
+            predictions["prediction"] == tempdf["prediction"])
+        temp_ids_df = cluster_id.select("player_id").collect()
+        temp_ids1 = []
+        for j in temp_ids_df:
+            j_dict = j.asDict()
+            temp_ids1.append(j_dict["player_id"])
+        tot = 0
+        for k in temp_ids1:
+            if k in player_ratings and k in player_coeff1:
+                tot += (player_ratings[k]*player_coeff1[k])
+        tot = tot/len(temp_ids_df)
+        tot1 += tot
 
+    for i in lessthan2:
+        tempdf = predictions.filter(predictions["player_id"] == i)
+        cluster_id = predictions.filter(
+            predictions["prediction"] == tempdf["prediction"])
+        temp_ids_df = cluster_id.select("player_id").collect()
+        temp_ids2 = []
+        for j in temp_ids_df:
+            j_dict = j.asDict()
+            temp_ids2.append(j_dict["player_id"])
+        tot = 0
+        for k in temp_ids2:
+            if k in player_ratings and k in player_coeff2:
+                tot += (player_ratings[k]*player_coeff2[k])
+
+        tot = tot/len(temp_ids_df)
+        tot2 += tot
+    strength_a = tot1/11
+    strength_b = tot2/11
+    chance_a = (0.5+strength_a-((strength_a+strength_b)/2))*100
+    chance_b = 100-chance_a
+    return chance_a, chance_b
